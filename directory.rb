ac5587297a0e647a7a9c50bf928ab58f08917be6
mavesonzini <mavesonzini@gmail.com>
2016-10-26 10:41:47 +0100

    Refactor and Iteration

--- directory.rb
+++ directory.rb
@@ -1,18 +1,23 @@
+#first we print the list of students
+students = ["Dr.Hannibal Lecter", "Darth Vader","Nurse Ratched",
+"Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",
+"Terminator", "Freddy Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
+
+def print_header
 puts "The students of Villains Academy"
 puts "----------------"
-puts "Dr.Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# Finally, we print the total number of students
-print "Overal, we have "
-#it's important that print() doesn't add a new line characters
-print 11
-puts " great students"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overal, we have #{names.count} great students "
+end
+#Nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
