dd377e3676cb4fbd3182fa5db78f7b32f82816d5
mavesonzini <mavesonzini@gmail.com>
2016-10-31 13:13:06 +0000

    Section 9

--- directory.rb
+++ directory.rb
@@ -13,6 +13,28 @@
 #  {name: "Norman Bates", cohort: :november}
 # ]
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        # print(students)
+        print_student(students)
+        print_footer(students)
+      when "9"
+        exit
+      else puts "I don't know what you meant, try again"
+      end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, please enter 'return' twice"
@@ -106,13 +128,8 @@ def print_footer(students)
 end
 #Nothing happens until we call the methods
 
-students = input_students
+
 # puts "If you want to print all cohorts write 'ALL'. If you want an specific cohort write the cohort month"
 # cohort_filter = gets.delete("\n")
 # if cohort_filter == "ALL"
-
-
-print_header
-# print(students)
-print_student(students)
-print_footer(students)
+interactive_menu
